1c46fe332f22dd464837fdd8f6db660ba9037f5c
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-09-05 16:29:31 -0300

    arruma retorno loginDTO

--- src/modules/user/dto/login-token.dto.input.ts
+++ src/modules/user/dto/login-token.dto.input.ts
@@ -1,13 +1,6 @@
-import { HttpStatus } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginTokenDTO {
   @ApiProperty()
   access_token: string;
-
-  @ApiProperty()
-  status: HttpStatus;
-
-  @ApiProperty()
-  errors?: string[];
 }
