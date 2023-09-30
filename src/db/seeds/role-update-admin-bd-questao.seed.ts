b878e2c435ca79c41f219203b217731336effa7d
danielhssouza <danielsoter3@gmail.com>
2023-09-30 17:06:50 -0300

    feature: adiciona BancoQuestoes nas roles

---
+++ src/db/seeds/role-update-admin-bd-questao.seed.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { RoleRepository } from 'src/modules/role/role.repository';
+
+@Injectable()
+export class RoleUpdateAdminSeedService {
+  constructor(private readonly roleRepository: RoleRepository) {}
+
+  async seed() {
+    await this.roleRepository
+      .update('admin', { bancoQuestoes: true })
+      .catch(() => {});
+  }
+}
