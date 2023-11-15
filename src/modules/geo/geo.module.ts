c5b7756fe2bf4ea909e83b6848fe1b785dd39bd9
FernandoAlmeidaPinto <fernando.almeida.pinto@gmail.com>
2023-11-15 17:23:29 -0300

    fix: ajusta update de questao e importacao de services

--- src/modules/geo/geo.module.ts
+++ src/modules/geo/geo.module.ts
@@ -6,10 +6,22 @@ import { EmailService } from 'src/shared/services/email.service';
 import { UserModule } from '../user/user.module';
 import { RoleModule } from '../role/role.module';
 import { AuditLogModule } from '../audit-log/audit-log.module';
+import { AuditLogRepository } from '../audit-log/audit-log.repository';
+import { AuditLogService } from '../audit-log/audit-log.service';
+import { UserRoleRepository } from '../user-role/user-role.repository';
+import { UserRoleService } from '../user-role/user-role.service';
 
 @Module({
   controllers: [GeoController],
   imports: [UserModule, RoleModule, AuditLogModule],
-  providers: [GeoService, GeoRepository, EmailService],
+  providers: [
+    GeoService,
+    GeoRepository,
+    EmailService,
+    UserRoleService,
+    UserRoleRepository,
+    AuditLogService,
+    AuditLogRepository,
+  ],
 })
 export class GeoModule {}
