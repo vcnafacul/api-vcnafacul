4ed43c3d37f2dda2e2abb3a39a355bf2ab602031
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-04-21 12:11:28 -0300

    refactor: ajusta cors prod

---
+++ src/config/cors.ts
@@ -0,0 +1,16 @@
+import {
+  CorsOptions,
+  CorsOptionsDelegate,
+} from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export function VcnafaculCors():
+  | boolean
+  | CorsOptions
+  | CorsOptionsDelegate<any> {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      origin: 'https://www.vcnafacul.com.br',
+    };
+  }
+  return true;
+}
