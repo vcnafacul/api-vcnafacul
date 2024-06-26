936c12ba8dd8ac6a237cd29b13ef52ced26f2b92
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-06-26 15:57:14 -0300

    refactor: refatora chamada de ms-simulado

--- src/modules/simulado/questao/questao.service.ts
+++ src/modules/simulado/questao/questao.service.ts
@@ -1,10 +1,8 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AxiosError } from 'axios';
-import { catchError, map } from 'rxjs';
 import { AuditLogService } from 'src/modules/audit-log/audit-log.service';
 import { User } from 'src/modules/user/user.entity';
+import { HttpServiceAxios } from 'src/shared/services/axios/httpServiceAxios';
 import { CreateQuestaoDTOInput } from '../dtos/create-questao.dto.input';
 import { QuestaoDTOInput } from '../dtos/questao.dto.input';
 import { UpdateDTOInput } from '../dtos/update-questao.dto.input';
@@ -13,12 +11,11 @@ import { Status } from '../enum/status.enum';
 @Injectable()
 export class QuestaoService {
   constructor(
-    private readonly http: HttpService,
+    private readonly axios: HttpServiceAxios,
     private readonly configService: ConfigService,
     private readonly auditLod: AuditLogService,
   ) {
-    this.http.axiosRef.defaults.baseURL =
-      this.configService.get<string>('SIMULADO_URL');
+    this.axios.setBaseURL(this.configService.get<string>('SIMULADO_URL'));
   }
 
   public async getAllQuestoes(query: QuestaoDTOInput) {
@@ -28,25 +25,11 @@ export class QuestaoService {
       baseUrl = baseUrl + `${key}=${query[key]}&`;
     });
 
-    return await this.http
-      .get(baseUrl)
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError((error: AxiosError) => {
-          throw error.response.data;
-        }),
-      );
+    return await this.axios.get(baseUrl);
   }
 
   public async questoesInfo() {
-    return await this.http
-      .get(`v1/questao/infos`)
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError((error: AxiosError) => {
-          throw error.response.data;
-        }),
-      );
+    return await this.axios.get(`v1/questao/infos`);
   }
 
   public async questoesUpdateStatus(
@@ -55,39 +38,18 @@ export class QuestaoService {
     user: User,
     message?: string,
   ) {
-    return await this.http
-      .patch(`v1/questao/${id}/${status}`, {
-        message,
-        userId: user.id,
-      })
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError((error: AxiosError) => {
-          throw error.response.data;
-        }),
-      );
+    return await this.axios.patch(`v1/questao/${id}/${status}`, {
+      message,
+      userId: user.id,
+    });
   }
 
   public async questoesUpdate(questao: UpdateDTOInput) {
-    return await this.http
-      .patch(`v1/questao`, questao)
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError((error: AxiosError) => {
-          throw error.response.data;
-        }),
-      );
+    return await this.axios.patch(`v1/questao`, questao);
   }
 
   public async createQuestion(questao: CreateQuestaoDTOInput) {
-    return await this.http
-      .post(`v1/questao`, questao)
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError((error: AxiosError) => {
-          throw error.response.data;
-        }),
-      );
+    return await this.axios.postR(`v1/questao`, questao);
   }
 
   public async uploadImage(file: any): Promise<string> {
@@ -98,13 +60,6 @@ export class QuestaoService {
   }
 
   public async delete(id: string) {
-    return await this.http
-      .delete(`v1/questao/${id}`)
-      .pipe(map((res) => res.data))
-      .pipe(
-        catchError((error: AxiosError) => {
-          throw error.response.data;
-        }),
-      );
+    await this.axios.delete(`v1/questao/${id}`);
   }
 }
