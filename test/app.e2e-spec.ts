5e869888397db604803c6807c5cc2641f5ab14a0
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-09-04 14:53:21 -0300

    feat: cria teste health base

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -15,10 +15,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('health', () => {
+    return request(app.getHttpServer()).get('/health').expect(200).expect('OK');
   });
 });
