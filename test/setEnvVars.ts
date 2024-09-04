f03853aa74ab53af73312b5de79d589005e42af0
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-09-04 15:27:20 -0300

    refactor: altera .env por arquivo ts env

---
+++ test/setEnvVars.ts
@@ -0,0 +1,10 @@
+process.env.HOST = '0.0.0.0';
+process.env.API_PORT = '3333';
+process.env.NODE_ENV = 'development';
+process.env.APP_KEY = 'vcnafacul_app_key';
+
+process.env.MY_HOST = 'localhost';
+process.env.MY_PORT = '3306';
+process.env.MY_USER = 'root';
+process.env.MY_PASSWORD = '1234';
+process.env.MY_DB_NAME = 'mysql';
