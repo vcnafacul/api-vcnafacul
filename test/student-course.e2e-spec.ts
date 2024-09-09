1e0abfcd326d65e801897e4bb56df10f147ce95c
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-09-08 22:00:29 -0300

    test: ajusta config teste e implementa testes de student course

--- test/student-course.e2e-spec.ts
+++ test/student-course.e2e-spec.ts
@@ -2,24 +2,19 @@ import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
-import { GeoService } from 'src/modules/geo/geo.service';
-import { PartnerPrepCourseDtoInput } from 'src/modules/prepCourse/partnerPrepCourse/dtos/create-partner-prep-course.input.dto';
 import { UserRepository } from 'src/modules/user/user.repository';
 import { UserService } from 'src/modules/user/user.service';
 import { JwtAuthGuard } from 'src/shared/guards/jwt-auth.guard';
 import { EmailService } from 'src/shared/services/email/email.service';
 import * as request from 'supertest';
-import { CreateGeoDTOInputFaker } from 'test/faker/create-geo.dto.input.faker';
-import { CreateUserDtoInputFaker } from 'test/faker/create.dto..input.faker';
+import { createStudentCourseDTOInputFaker } from './faker/create-student-course.dto.input.faker';
+import { CreateUserDtoInputFaker } from './faker/create.dto..input.faker';
+import { createNestAppTest } from './utils/createNestAppTest';
 
-// Mock the EmailService globally
-jest.mock('src/shared/services/email/email.service');
-
-describe('PartnerPrepCourse (e2e)', () => {
+describe('StudentCourse (e2e)', () => {
   let app: INestApplication;
   let userService: UserService;
   let userRepository: UserRepository;
-  let geoService: GeoService;
   let emailService: EmailService;
 
   beforeEach(async () => {
@@ -27,16 +22,15 @@ describe('PartnerPrepCourse (e2e)', () => {
       imports: [AppModule],
       providers: [EmailService, ConfigService],
     })
-      .overrideGuard(JwtAuthGuard) // Aqui estamos substituindo o guard por um mock
+      .overrideGuard(JwtAuthGuard) // Here we are replacing the guard with a mock
       .useValue({
         canActivate: jest.fn(() => true),
       })
       .compile();
 
-    app = moduleFixture.createNestApplication();
+    app = createNestAppTest(moduleFixture);
     userService = moduleFixture.get<UserService>(UserService);
     userRepository = moduleFixture.get<UserRepository>(UserRepository);
-    geoService = moduleFixture.get<GeoService>(GeoService);
     emailService = moduleFixture.get<EmailService>(EmailService);
 
     jest
@@ -50,23 +44,32 @@ describe('PartnerPrepCourse (e2e)', () => {
     await app.close();
   });
 
-  it('should create a new PartnerPrepCourse', async () => {
-    const geoDto = CreateGeoDTOInputFaker();
-    const geo = await geoService.create(geoDto);
-
+  it('should create a new StudentCourse', async () => {
     const userDto = CreateUserDtoInputFaker();
     await userService.createUser(userDto);
     const user = await userRepository.findOneBy({ email: userDto.email });
 
-    const dto: PartnerPrepCourseDtoInput = { geoId: geo.id, userId: user.id };
+    const dto = createStudentCourseDTOInputFaker(user.id);
 
     return request(app.getHttpServer())
-      .post('/partner-prep-course')
+      .post('/student-course')
       .send(dto)
-      .expect(201)
+      .expect(400)
       .expect((res) => {
-        expect(res.body.geoId).toEqual(geo.id);
-        expect(res.body.userId).toEqual(user.id);
+        console.log(res.body);
+        expect(res.body.id).not.toBeNull();
       });
   }, 30000);
+
+  it('user not exist', async () => {
+    const dto = createStudentCourseDTOInputFaker();
+
+    return request(app.getHttpServer())
+      .post('/student-course')
+      .send(dto)
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.id).not.toBeNull();
+      });
+  }, 300000);
 });
