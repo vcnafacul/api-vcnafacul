6e04d569fe180f243836a3ace2368dcd0cf445dd
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-09-12 12:43:11 -0300

    fix: add provider dependência faltante

--- src/modules/prepCourse/studentCourse/student-course.module.ts
+++ src/modules/prepCourse/studentCourse/student-course.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BlobModule } from 'src/shared/services/blob/blob.module';
+import { DocumentStudentRepository } from './documents/document-students.repository';
 import { StudentCourseController } from './student-course.controller';
 import { StudentCourseRepository } from './student-course.repository';
 import { StudentCourseService } from './student-course.service';
@@ -7,7 +8,11 @@ import { StudentCourseService } from './student-course.service';
 @Module({
   controllers: [StudentCourseController],
   imports: [BlobModule],
-  providers: [StudentCourseService, StudentCourseRepository],
+  providers: [
+    StudentCourseService,
+    StudentCourseRepository,
+    DocumentStudentRepository,
+  ],
   exports: [StudentCourseService, StudentCourseRepository],
 })
 export class StudentCourseModule {}
