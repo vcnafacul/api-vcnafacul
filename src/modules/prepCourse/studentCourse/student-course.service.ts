3ed849ae8fbbcac0f3ab11b9d2ee51c166f1e4b2
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2024-12-19 10:47:33 -0300

    refactor: refatora log documento enviado para ser realizado somente 1 vez

--- src/modules/prepCourse/studentCourse/student-course.service.ts
+++ src/modules/prepCourse/studentCourse/student-course.service.ts
@@ -171,14 +171,14 @@ export class StudentCourseService extends BaseService<StudentCourse> {
         document.studentCourse = student.id;
 
         await this.documentRepository.create(document);
-
-        const log = new LogStudent();
-        log.studentId = student.id;
-        log.applicationStatus = StatusApplication.SendedDocument;
-        log.description = 'Documento enviado';
-        await this.logStudentRepository.create(log);
       }),
-    );
+    ).then(async () => {
+      const log = new LogStudent();
+      log.studentId = student.id;
+      log.applicationStatus = StatusApplication.SendedDocument;
+      log.description = 'Documento enviado';
+      await this.logStudentRepository.create(log);
+    });
   }
 
   async getDocument(fileKey: string) {
@@ -503,7 +503,7 @@ export class StudentCourseService extends BaseService<StudentCourse> {
     return false;
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
+  @Cron(CronExpression.EVERY_MINUTE, {
     timeZone: 'America/Sao_Paulo',
   })
   async sendEmailDeclaredInterest() {
