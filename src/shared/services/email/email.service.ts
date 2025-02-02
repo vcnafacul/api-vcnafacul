9276d7293b75f588ef7cd3e9408e2b9451d0c2d1
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2025-02-02 15:21:20 -0300

    refactor: altera envio de email "to" para "bbc"

--- src/shared/services/email/email.service.ts
+++ src/shared/services/email/email.service.ts
@@ -63,7 +63,7 @@ export class EmailService {
   async sendCreateGeoMail(geo: Geolocation, listEmail: string[]) {
     const info = await this.transporter.sendMail({
       from: this.configService.get<string>('SMTP_USERNAME'),
-      to: listEmail,
+      bcc: listEmail,
       subject: 'Cadastro de Cursinho',
       html: htmlGeo(geo),
       // Você também pode usar HTML aqui
@@ -160,7 +160,7 @@ export class EmailService {
 
     const mailOptions = {
       from: this.configService.get<string>('SMTP_USERNAME'),
-      to: emails,
+      bcc: emails,
       subject: `Atualização Lista de Espera ${prepCourseName} - Você na Facul`,
       context: {
         students,
