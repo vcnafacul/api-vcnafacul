09d0f3e8abb677d447357ae24703e7620ba4c1fc
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2025-03-04 11:43:55 -0300

    refactor: Ajusta detalhes #261

--- src/db/migrations/1741099369597-attendance_record.ts
+++ src/db/migrations/1741099369597-attendance_record.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AttendanceRecord1741050472620 implements MigrationInterface {
-  name = 'AttendanceRecord1741050472620';
+export class AttendanceRecord1741099369597 implements MigrationInterface {
+  name = 'AttendanceRecord1741099369597';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -11,7 +11,7 @@ export class AttendanceRecord1741050472620 implements MigrationInterface {
       `CREATE TABLE \`attendance_record\` (\`id\` varchar(36) NOT NULL, \`created_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updated_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`deleted_at\` timestamp NULL, \`registeredAt\` date NOT NULL, \`classId\` varchar(36) NULL, \`registeredById\` varchar(36) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`student_attendance\` (\`id\` varchar(36) NOT NULL, \`created_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updated_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`deleted_at\` timestamp NULL, \`present\` tinyint(1) NOT NULL, \`studentCourseId\` varchar(36) NULL, \`attendanceRecordId\` varchar(36) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`student_attendance\` (\`id\` varchar(36) NOT NULL, \`created_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updated_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`deleted_at\` timestamp NULL, \`present\` tinyint(1) NOT NULL, \`studentCourseId\` varchar(36) NULL, \`attendanceRecordId\` varchar(36) NULL, UNIQUE INDEX \`IDX_2d5ea3fa66a07b9e95a86563e1\` (\`attendanceRecordId\`, \`studentCourseId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
       `ALTER TABLE \`attendance_record\` ADD CONSTRAINT \`FK_120988e6adf02eb67c14330eb57\` FOREIGN KEY (\`classId\`) REFERENCES \`classes\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -39,6 +39,9 @@ export class AttendanceRecord1741050472620 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE \`attendance_record\` DROP FOREIGN KEY \`FK_120988e6adf02eb67c14330eb57\``,
+    );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_2d5ea3fa66a07b9e95a86563e1\` ON \`student_attendance\``,
     );
     await queryRunner.query(`DROP TABLE \`student_attendance\``);
     await queryRunner.query(`DROP TABLE \`attendance_record\``);
--- src/modules/prepCourse/attendance/studentAttendance/student-attendance.entity.ts
+++ src/modules/prepCourse/attendance/studentAttendance/student-attendance.entity.ts
@@ -1,10 +1,11 @@
-import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToOne, Unique } from 'typeorm';
 import { BaseEntity } from '../../../../shared/modules/base/entity.base';
 import { StudentCourse } from '../../studentCourse/student-course.entity';
 import { AbsenceJustification } from '../absenceJustification/absence-justification.entity';
 import { AttendanceRecord } from '../attendanceRecord/attendance-record.entity';
 
 @Entity('student_attendance')
+@Unique(['attendanceRecord', 'studentCourse'])
 export class StudentAttendance extends BaseEntity {
   @ManyToOne(() => StudentCourse, (studentCourse) => studentCourse.attendance)
   public studentCourse: StudentCourse;
